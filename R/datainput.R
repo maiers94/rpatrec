1ac5f991e007950866d78a5cc2e6c97d17f88fa1
maiers94 <stephan_mar_maier@yahoo.co.uk>
2017-02-17 16:03:32 +0000

    added data handling function

---
+++ R/datainput.R
@@ -0,0 +1,30 @@
+#library(openxlsx)
+
+
+# data <- read.xlsx("ds_new.xlsx",2)
+# milan <- data[[1]]
+# sandp.tsx <- data[[2]]
+# dax <- data[[3]]
+# dow <- data[[4]]
+# nikkei <-data[[5]]
+
+#'Prepare Data for using with the other functions by removing non-numeric elements, and removing repeated values
+
+sample.pre <- function(input){
+  i <- 1
+  while(is.numeric(input[i])==FALSE){
+    input <- input[(i+1):length(input)]
+    i <- i+1
+    print(i)
+  }
+  output <- input[1]
+  cut <- 0
+  for(i in 2:length(input)){
+    if(input[i-1]!=input[i]){
+      output <- c(output,input[i])
+    }
+    else cut <- cut +  1
+  }
+  print(c("cut input by",cut))
+  return(output)
+}
