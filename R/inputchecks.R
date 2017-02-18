0a1fcf896a49d130766175b168ed66581a6c6d07
maiers94 <stephan_mar_maier@yahoo.co.uk>
2017-02-18 16:28:49 +0000

    Adding error checking capabilities

---
+++ R/inputchecks.R
@@ -0,0 +1,92 @@
+#this function provides input checks for other functions in this package. The idea is to have a centralised input check as this
+#should produce more efficient code
+
+#arg: vector of arguments
+#who: name of the calling function
+inputchecks <- function(arg,who){
+
+
+  if(who=="generator"){
+    #arg 1 needs to be numeric
+    #arg 2,3 need to be numeric and greater than 0
+    #arg 4,5 need be non-negative integers
+    #arg 6 needs to be integer and grater than 0
+    #arg 7 needs to be a vector of length arg 6 +2, first element 0, last 100
+    #arg 8 needs to be a vector of length arg 6 +2, first element 0, last 0
+    singlenumber(arg[[1]])
+    singlenumber(arg[[2]],pos=TRUE)
+    singlenumber(arg[[3]],pos=TRUE)
+    singlenumber(arg[[4]],nneg=TRUE,int=TRUE)
+    singlenumber(arg[[5]],nneg=TRUE,int=TRUE)
+    singlenumber(arg[[6]],pos=TRUE,int=TRUE)
+    plus2 <- arg[[6]]+2
+    vectornumber(arg[[7]],plus2)
+    singlenumber(arg[[7]][1],int=TRUE,specific=0)
+    singlenumber(arg[[7]][plus2],int=TRUE,specific=100)
+    vectornumber(arg[[8]],plus2)
+    singlenumber(arg[[7]][1],int=TRUE,specific=0)
+    singlenumber(arg[[7]][plus2],int=TRUE,specific=0)
+  }
+  if(who=="noise"){
+    #arg1 needs to be a numeric vector
+    #arg2 needs to be "white", "red", or "var"
+    #arg3 needs to be numeric  positive
+    vectornumber(arg[[1]])
+    correctstring(arg[[2]],c("white","red","var"))
+    singlenumber(arg[[3]],pos = TRUE)
+  }
+  if(who=="sample.pre"){
+    vectornumber(arg[[1]])
+  }
+  if(who=="mav"){
+    vectornumber(arg[[1]])
+    singlenumber(arg[[2]],pos=TRUE,int=TRUE)
+    correctstring(arg[[3]],specific=c("simple","median","exponential"))
+  }
+
+  return(0)
+}
+
+singlenumber <- function(x,int=FALSE,pos=FALSE,nneg=FALSE,specific=FALSE){
+  if(!is.numeric(x))stop("invalid arguments - not numeric")
+  if(length(x)!=1)stop("invalid arguments - not of length 1")
+  if(int==TRUE){
+    if(x%%1!=0)stop("invalid arguments - not an integer")
+  }
+  if(pos==TRUE){
+    if(x<=0)stop("invalid arguments - not positive")
+  }
+  if(nneg==TRUE){
+    if(x<0)stop("invalid arguments - not non-negative")
+  }
+  if(specific!=FALSE){
+    if(x!=specific)stop("invalid arguments")
+  }
+  return(0)
+}
+vectornumber <- function(x,l=FALSE){
+  if(l!=FALSE){
+    if(length(x)!=l)stop("invalid arguments - not of correct length")
+  }
+  if(!is.numeric(x))stop("invalid arguments - not numeric")
+  return(0)
+}
+
+correctstring <- function(x,specific=FALSE){
+  if(!is.character(x))stop("invalid arguments")
+  if(specific[1]!=FALSE){
+    if(length(specific)==1){
+      if(x!=specific)stop("invalid arguments")
+    }
+    else{
+      check <- 0
+      for(i in 1:length(specific)){
+        if(x==specific[i])check <- 1
+      }
+      if(check==0)stop("invalid arguments")
+    }
+  }
+  return(0)
+}
+
+
