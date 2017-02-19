5fe38e098f5073dcdf263eb7f11423c4142f38aa
maiers94 <stephan_mar_maier@yahoo.co.uk>
2017-02-19 00:51:58 +0000

    improved tests

--- tests/testthat/testnoise.R
+++ tests/testthat/testnoise.R
@@ -1,8 +1,16 @@
 library(rpatrec)
 context("Output_of_noise")
 
-test_that("output length is the same as input length",{
+test_that("output given standard inputs",{
   a <- seq(1:100)
   expect_equal(length(noise(a,"white",2)),length(a))
+  expect_error(noise("ASD","white",2))
+})
 
+test_that("output standard deviation is as required",{
+  a <- seq(1:10000)
+  b <- noise(a,"white",1)
+  c <- sd(b-a)
+  d <- (c > 0.85 && c < 1.15)
+  expect_true(d)
 })
