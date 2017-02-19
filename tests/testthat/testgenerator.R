fd8e1e2fd885cf598647ef10fdde6f371f0ae1ae
maiers94 <stephan_mar_maier@yahoo.co.uk>
2017-02-19 01:13:03 +0000

    more tests

--- tests/testthat/testgenerator.R
+++ tests/testthat/testgenerator.R
@@ -7,3 +7,10 @@ test_that("Generator produces a timeseries with the right extrema",{
   expect_lte(a[25],81)
   expect_gte(a[25],79)
 })
+
+test_that("pre and post-signal are correct",{
+  a <- generator(presig=50,postsig=50,plength=3,parts=c(0,25,50,75,100),sprd=c(0,80,40,80,0))
+  expect_length(a,200)
+  expect_equal(a[4],0)
+  expect_equal(a[175],a[176])
+})
