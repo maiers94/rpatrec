a770768be745956db5e0e18edcba336bfe6e38f1
maiers94 <stephan_mar_maier@yahoo.co.uk>
2017-03-05 22:51:00 +0000

    important bugfixes

--- R/Testfunction.R
+++ R/Testfunction.R
@@ -8,42 +8,50 @@
 #'@param ... other parameters the smoother requires
 #'
 #'@export
-test.smoother <- function(n=1,m=5,incr=1,max=100,smoother,...){
-  result <- vector()
+test.smoother <- function(n=1,m=5,incr=1,max=20,smoother,...){
+  result <- rep(0,max)
   for(i in 1:n){
     pat <- generator()
     r <- 1
     zero <- 0
     nresult <- vector()
-    while(r <= max && zero < 3){
-      curerror <- (r - 1) * incr
+    while(r <= max && zero < (3 *m) ){
+      curerror <- r * incr
+      #print(curerror)
       count <- 0
       for(j in 1:m){
         epat <- noise(pat,"white",curerror)
         spat <- smoother(input = epat,...)
         reci <- interpret(spat)
-        rec <- reci[[4]][[1]]
+        rec <- reci[[3]]
         #### if checks
         #check whether the pattern has been recognised
-        if(rec[1] > 50*0.95 && cur[1] < 50 *1.05){
-          if(rec[2] > 25*0.95 && cur[2] < 25 *1.05){
-            if(rec[3] > 100*0.95 && cur[3] < 100 *1.05){
-              if(rec[4] > 25*0.95 && cur[4] < 25 *1.05){
-                if(rec[5] > 50*0.95 && cur[5] < 50 *1.05){
-                  status <- TRUE
+        #print(rec)
+        status <- FALSE
+        if(rec[1] > 12 && rec[1] < 18){
+          if(rec[2] > 22 && rec[2] < 28){
+            if(rec[3] > 47 && rec[3] < 53){
+              if(rec[4] > 72 && rec[4] < 78){
+                if(rec[5] > 82 && rec[5] < 88){
+                  if(length(rec)==5)status <- TRUE
                 }
               }
             }
           }
         }
-        else status <- FALSE
         #### end if checks
-        if(status == TRUE)count <- count + 1
+        if(status == TRUE){
+          count <- count + 1
+          zero <- 0
+        }
+        else zero <- zero + 1
       }
       nresult[r] <- count / m * 100
       r <- r + 1
     }
-    result <- result  + nresult
+    for(k in 1:length(nresult)){
+      result[k] <- nresult[k] + result[k]
+    }
   }
 
   result <- result / n
