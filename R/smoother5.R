0c8bf84c5c626e226493eba92d5947ed71a08548
maiers94 <stephan_mar_maier@yahoo.co.uk>
2017-04-11 15:06:30 +0100

    splines added

--- R/smoother5.R
+++ R/smoother5.R
@@ -1,1 +1,14 @@
-#splines
+#'Uses the inbuilt function \code{smooth.spline()} to smooth the data using splines.
+#'
+#'This function is purely included to provide the standard interface coherent with other smoothers to the user.
+#'
+#' @param input Time series data passed to \code{smooth.spline()}
+#' @param ... Optional: Other arguments passed to \code{smooth.spline()}
+#'
+#' @return Smoothed time series data only, no additional output. Use \code{smooth.spline()} for greater functionality
+#' @export
+
+splines <- function(input,...){
+  result <- smooth.spline(x=input,...)
+  return(result[[2]])
+}
